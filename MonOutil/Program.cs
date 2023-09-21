a50b452f0f9517b2a8106c64d758f467d14b6f19
bstuff <arthur.bohn@lcieducation.net>
2023-09-21 11:43:20 -0400

    Ajout programme

---
+++ MonOutil/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bonjour, veuillez entrez votre nom d'utilisateur :");
+string nom=Console.ReadLine();
+Console.WriteLine("Bienvenue "+nom+" à l'application MonOutil.");
+Console.WriteLine("Entrez une température en Celsius :");
+double C = Convert.ToDouble (Console.ReadLine());
+double F = C * 1.8;
+F = F +32;
+Console.WriteLine("Cette température est de " + F + " degrés Fahrenheit.");
+Console.ReadKey();
